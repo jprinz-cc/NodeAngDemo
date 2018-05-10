e1e1d8b9cc3e772e00117faa4fc0e5372cbf1c5b
jprinz <jprinz@conestogac.on.ca>
2018-05-10 14:56:23 -0400

    Initial commint

---
+++ server.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
+
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+
+
+
+function send404(response) {
+    /*response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('Error 404: Resource not found.');
+    response.end();*/
+    response.writeHead(200, { 'content-type': 'text/html' });
+    fs.createReadStream('./public/404.html').pipe(response);
+    util.log("404 Served")
+}
+
+var mimeLookup = {
+    '.js': 'application/javascript',
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.gif': 'image/gif',
+    '.jpg': 'image/jpg',
+    '.ico': 'image/x-icon'
+};
+
+var server = http.createServer(function (req, res) {
+    if(req.method == 'GET') {
+        var fileurl;
+        if(req.url == '/') {
+            fileurl = '/index.html';
+        } else {
+            fileurl = req.url;
+        }
+
+        var filepath = path.resolve('./public' + fileurl);
+        util.log("fileurl: ", fileurl);
+
+        var fileExt = path.extname(filepath);
+        var mimeType = mimeLookup[fileExt];
+
+        if(!mimeType) {
+            send404(res);
+            return;
+        }
+
+        fs.exists(filepath, function (exists){
+            if(!exists) {
+                send404(res);
+                return;
+            };
+
+            res.writeHead(200, { 'content-type': mimeType });
+            fs.createReadStream(filepath).pipe(res);
+            util.log("Resource Served");
+        });
+    } else {
+        send404(res);
+    }
+}).listen(port, ipaddr);
+util.log('Server running on port', port, 'and IP', ipaddr);
